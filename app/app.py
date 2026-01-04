7aee12bbeffc8455f43662a38bf562fd04feb1d8
marcusayan <ayan988ahmad@gmail.com>
2026-01-05 01:26:55 +0530

    file section completed

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
 from app.schemas import PostCreate, PostResponse
 from app.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+from sqlalchemy import select
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -12,25 +13,44 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-text_posts = {
-    1: {"title": "New Post", "content": "cool test post"},
-    2: {"title": "Python Tip", "content": "Use list for cleaner loops."}
-    }
+@app.post("/upload")
+async def upload_file(
+    file: UploadFile = File(...),
+    caption: str = Form(""),
+    
+    session: AsyncSession = Depends(get_async_session)
+    
+):
+    post = Post(
+        caption=caption,
+        url="dummy url",
+        file_type="photo",
+        file_name="dummy name"
+    )
+    session.add(post)
+    await session.commit()
+    await session.refresh(post)
+    return post
 
-@app.get("/posts") ## This fetches all the posts
-def get_all_posts():
-    return text_posts
 
-@app.get("/posts/{id}") ## This fetches only particular id posts
-def get_post(id: int) -> PostResponse:
-    if id not in text_posts:
-        raise HTTPException(status_code=404, detail="POST not found")
-    return text_posts.get(id)
-
-@app.post("/posts")
-def create_post(post: PostCreate) -> PostResponse:
-    new_post = {"title": post.title, "content": post.content}
-    text_posts[max(text_posts.keys()) + 1] = new_post
-    return new_post
-
-## PostResponse - It creates a validation that only that type of data is allowed
+ ## Retrieving Data
+@app.get("/feed")
+async def get_feed(
+    ## This is dependecncy injection
+    session:AsyncSession = Depends(get_async_session)
+):
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.all()]
+    posts_data = []
+    for post in posts:
+        posts_data.append(
+            {
+                "id": str(post.id),
+                "caption": post.caption,
+                "url": post.url,
+                "file_type": post.file_type,
+                "file_name": post.file_name,
+                "created_at": post.created_at.isoformat()
+            }
+        )
+    return {"posts": posts_data}
