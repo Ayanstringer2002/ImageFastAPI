f2b1a1cf23fd512faee33e0e4dd859ff6dc49c42
marcusayan <ayan988ahmad@gmail.com>
2026-01-04 23:35:30 +0530

    add Post method

--- app/app.py
+++ app/app.py
@@ -18,3 +18,8 @@ def get_post(id: int):
         raise HTTPException(status_code=404, detail="POST not found")
     return text_posts.get(id)
 
+@app.post("/posts")
+def create_post(post: PostCreate):
+    new_post = {"title": post.title, "content": post.content}
+    text_posts[max(text_posts.keys()) + 1] = new_post
+    return new_post
