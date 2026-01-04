1232893607d58b08850a0931ceccbc6ab51654a1
marcusayan <ayan988ahmad@gmail.com>
2026-01-04 22:44:52 +0530

    get functionalty added

---
+++ app/app.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, HTTPException
+from app.schemas import PostCreate
+
+app = FastAPI()
+
+text_posts = {
+    1: {"title": "New Post", "content": "cool test post"},
+    2: {"title": "Python Tip", "content": "Use list for cleaner loops."}
+    }
+
+@app.get("/posts") ## This fetches all the posts
+def get_all_posts():
+    return text_posts
+
+@app.get("/posts/{id}") ## This fetches only particular id posts
+def get_post(id: int):
+    if id not in text_posts:
+        raise HTTPException(status_code=404, detail="POST not found")
+    return text_posts.get(id)
+
